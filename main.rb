5a4ca1b77b2121fe349a1da21d7d36dc15a17f4c
G_Viktor <122882927+FantViktor@users.noreply.github.com>
2023-03-21 20:05:20 +0600

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,11 @@
+require_relative 'station'
+require_relative 'train'
+require_relative 'route'
+require_relative 'passenger_train'
+require_relative 'cargo_train'
+require_relative 'passenger_wagon'
+require_relative 'freight_wagon'
+require_relative 'wagon'
+require_relative 'interface'
+
+Interface.new.start
